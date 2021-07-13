bbb0ac6509d656841dc4b878c6c6b1c7d437b8e2
Fsesnick <fernandasesnick@gmail.com>
2021-07-13 00:34:58 -0300

    saving localstorage

--- src/App.js
+++ src/App.js
@@ -4,22 +4,29 @@ import './App.css';
 class App extends Component{
 
   state = {
-    email:""
+    email:" "
   };
 
   handleUserInput = e =>
   this.setState({ email: e.target.value});
 
+ addItem = e  => {
+  e.preventDefault();
+    let emailUser = this.state.email;
+    localStorage.setItem('email',emailUser );
+  };
+
 render() {
+  console.log(this.state.email);
   return (
       <div className="App">
          <header>
           <h1>Random Store</h1>
           <p>Random Fashion Co.</p>
-          <form>
+          <form onSubmit={this.addItem}>
               <input  
                   type="text"
-                  onChange = {this.handleUserInput}
+                  onChange = {this.handleUserInput}                  
                   value={this.state.email}
                   placeholder="Subscribe to newsletter"/>
           <button type="submit" name="submit" value="submit">Submit</button>
